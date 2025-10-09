90864556aece85455d99357858bb119e7db243c9
BatsCrow <twilliamsa776@gmail.com>
2025-10-09 18:51:04 -0400

    Made Directories for Requested Pages

---
+++ src/app/2025/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function SponsorPage() {
+
+    return (
+        <main>
+            <h1>This is the 2025 Page</h1>
+        </main>
+    )
+
+}
---
+++ src/app/awards/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function AwardsPage() {
+
+    return (
+        <main>
+            <h1>This is the awards Page</h1>
+        </main>
+    )
+
+}
