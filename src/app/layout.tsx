31aaf9d7d7b69e900eab9cec9e5de2741289fd83
BatsCrow <twilliamsa776@gmail.com>
2025-10-09 20:51:23 -0400

    Feature: Added navbar and footer to layout, added hero to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MainNavbar } from "@/components/MainNavbar";
+import { MainFooter } from "@/components/MainFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,7 +77,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MainNavbar />
         {children}
+        <MainFooter />
       </body>
     </html>
   );
