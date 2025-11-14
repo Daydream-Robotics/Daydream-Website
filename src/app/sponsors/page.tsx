396b5d29db79865f0a82244f7492d9b2fada1196
ethanfu868-creator <ethanfu868@gmail.com>
2025-11-13 21:17:12 -0500

    Update page.tsx

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -4,8 +4,13 @@ export default function SponsorPage() {
 
     return (
         <main>
+            
             <h1>This is the sponsor Page</h1>
+            <p className="m-auto self text-center">Insert Sponser name</p>
+            <div>
+            <img src ="globe.svg" alt = "globe icon"></img>
+            <img src ="globe.svg" alt = "globe icon"></img>
+            </div>
         </main>
     )
-
 }
